e5050c104e9f075f2b863106ba601f1957b72179
DESKTOP-UAM837D\Harika <mpalaniswamy@prokarma.com>
2022-12-08 11:06:22 +0530

    flask_01

---
+++ flask/app.py
@@ -0,0 +1,29 @@
+from flask import Flask,redirect,url_for
+### WSGI Application
+app = Flask(__name__)
+
+@app.route('/') ### Decoreator
+def welcome():   ## Decorator come with a function
+    return "Welcome Flask toout "
+
+@app.route('/success/<int:score>') ### String  <score>
+def success(score):   ## Decorator come with a function
+    return "The Person has passed and the marks is  "+str(score)
+
+@app.route('/fail/<int:score>') ### String  <score>
+def fail(score):   ## Decorator come with a function
+    return "The Person has failed and the marks is  "+str(score)
+
+
+@app.route('/results/<int:marks>') ### String  <score>
+def results(marks):   ## Decorator come with a function
+    results=""
+    if marks<50:
+        results = 'fail'
+    else:
+        results = 'success'
+    ###return "The Person has "+ results +" and the marks is  "+str(score)
+    return redirect(url_for(results,score=marks))
+
+if __name__=='__main__':
+    app.run(debug=True)
